46c49e37c00557416f40d35badca7efeef15afac
Jean de Sá <jean.sa@celfinet.com>
2022-10-23 14:54:42 +0100

    first test complete.

--- calculator/src/test/java/com/calculator/entities/CalculatorTests.java
+++ calculator/src/test/java/com/calculator/entities/CalculatorTests.java
@@ -1,19 +1,48 @@
 package com.calculator.entities;
 
 import org.junit.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 
 public class CalculatorTests {
-        @Test
-        public void shouldSumTwoValues(){
-            int a = 0;
-            int b = 0;
+    @Test
+    public void shouldSumTwoValues(){
+            int a = 4;
+            int b = 6;
             Sum sum = new Sum();
-            double expectedResult = 0.0;
+            double expectedResult = 10;
             double result = sum.totalSum(a, b);
 
-            assertEquals(expectedResult,result);
-            fail("The test is a prototype.");
+            assertEquals(expectedResult,result,0);
         }
+    @Test
+    public void shouldSubtractTwoValues() {
+        int n1 = 5;
+        int n2 = 3;
+        Subtract subtract = new Subtract();
+        double expResult = 2;
+        double result = subtract.totalSubtraction(n1,n2);
+
+        assertEquals(expResult, result, 0);
+    }
+    @Test
+    public void shouldMultiplyTwoValues() {
+        int n1 = 2;
+        int n2 = 3;
+        Multiply multiply = new Multiply();
+        double expResult = 6;
+        double result = multiply.totalMultiplier(n1,n2);
+
+        assertEquals(expResult, result, 0);
+    }
+    @Test
+    public void shouldDivideTwoValues() {
+        int n1 = 5;
+        int n2 = 2;
+        Division division = new Division();
+        double expResult = 2.5;
+        double result = division.totalDivision(n1,n2);
+
+        assertEquals(expResult, result, 0);
+    }
     }
